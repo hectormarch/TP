ba5c6b2919e5708a85614de14012c1d1733e61ab
Hector <araujomarchena@gmail.com>
2022-10-18 03:59:08 -0300

    Modificando con JS

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let compra= document.getElementById('compra');
 
 let pagina_compra=document.getElementById('pagina').classList;
 let comprando=document.getElementById('comprando').classList;
+let result=document.getElementById('result').classList;
+let resultado=document.getElementById('resumen_button');
+const tipo_de_entrada=document.getElementById('inputState').value;
+let cantidad=document.getElementById('cantidad').value;
 
 
 function compraT(){
@@ -12,3 +16,17 @@ function compraT(){
 
 compra.addEventListener("click", compraT)
 
+resultado.addEventListener("click", resumen)
+
+function resumen(){
+    comprando.add("compratick")
+    result.remove("compratick")
+    if (document.getElementById('inputState').value==1){
+        document.getElementById('valor').innerHTML=200*0.2*document.getElementById('cantidad').value;
+    } else if(document.getElementById('inputState').value==2){
+        document.getElementById('valor').innerHTML=200*0.5*document.getElementById('cantidad').value;
+    } else {
+        document.getElementById('valor').innerHTML=200*0.85*document.getElementById('cantidad').value;
+    }
+
+}
