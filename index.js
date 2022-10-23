220551dd8af44756a85b7cc236eb4ba80db8ab67
Hector <araujomarchena@gmail.com>
2022-10-23 10:16:39 -0300

    Modificación de Total a Pagar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let compra= document.getElementById('compra');
 
-let pagina_compra=document.getElementById('pagina').classList;
+let pagina=document.getElementById('pagina').classList;
 let comprando=document.getElementById('comprando').classList;
 let result=document.getElementById('result').classList;
 let resultado=document.getElementById('resumen_button');
@@ -10,9 +10,10 @@ let cantidad=document.getElementById('cantidad').value;
 
 
 function compraT(){
-    pagina_compra.add("compratick")
+    pagina.add("compratick")
     comprando.remove("compratick")
 }
+
 
 compra.addEventListener("click", compraT)
 
@@ -22,11 +23,12 @@ function resumen(){
     comprando.add("compratick")
     result.remove("compratick")
     if (document.getElementById('inputState').value==1){
-        document.getElementById('valor').innerHTML=200*0.2*document.getElementById('cantidad').value;
+        document.getElementById('total').innerHTML=200*0.2*document.getElementById('cantidad').value;
     } else if(document.getElementById('inputState').value==2){
-        document.getElementById('valor').innerHTML=200*0.5*document.getElementById('cantidad').value;
+        document.getElementById('total').innerHTML=200*0.5*document.getElementById('cantidad').value;
     } else {
-        document.getElementById('valor').innerHTML=200*0.85*document.getElementById('cantidad').value;
+        document.getElementById('total').innerHTML=200*0.85*document.getElementById('cantidad').value;
     }
+    resultado.addClass('disabled')
 
 }
